10a61fe5c3bef5f5a8894e1a1365d3d76518d80f
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-22 12:33:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.addEventListener("submit", (event) => {
-//     let newTodo = document.getElementById("new-task-description").value;
-//     const list = document.getElementById("tasks");
-//     let item = document.createElement("li");
-//     item.innerText = newTodo;
-//     list.appendChild(item);
-//     event.preventDefault();
-//   });
-// });
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form');
   
