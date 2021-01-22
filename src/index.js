f95df22065ce098d3f1a5a04d0095425ceb8f488
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-22 12:33:13 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    let input = document.getElementById('new-task-description');
+    let inputValue = input.value;
+    
+    let liItem = document.createElement('li');
+    
+    let ulAll = document.querySelector('ul');
+    if (inputValue != '') {
+      liItem.innerText = inputValue;
+      ulAll.append(liItem);
+      input.value = '';
+    };
+    
+  })
 });
