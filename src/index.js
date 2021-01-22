abfd1116f5f4875e95c49d038d5ae3e8a507500b
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-22 12:31:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.addEventListener("submit", (event) => {
+//     let newTodo = document.getElementById("new-task-description").value;
+//     const list = document.getElementById("tasks");
+//     let item = document.createElement("li");
+//     item.innerText = newTodo;
+//     list.appendChild(item);
+//     event.preventDefault();
+//   });
+// });
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    let input = document.getElementById('new-task-description');
+    let inputValue = input.value;
+    
+    let liItem = document.createElement('li');
+    
+    let ulAll = document.querySelector('ul');
+    if (inputValue != '') {
+      liItem.innerText = inputValue;
+      ulAll.append(liItem);
+      input.value = '';
+    };
+    
+  })
 });
